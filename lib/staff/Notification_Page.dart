92d79af975976853567dc3f13fdcf4a11c3a8b7b
Nadiyah Shofa Salsabila <166197389+NadiyahShofaSalsabila@users.noreply.github.com>
2024-07-06 13:41:48 +0700

    Update Notification_Page.dart

---
+++ lib/staff/Notification_Page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NotificationPage extends StatelessWidget {
+  const NotificationPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Text('Coming Soon'),
+    ));
+  }
+}
