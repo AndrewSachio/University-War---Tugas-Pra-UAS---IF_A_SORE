324c9820bbb38da8a23e77ac685675a31870a53f
Nadiyah Shofa Salsabila <166197389+NadiyahShofaSalsabila@users.noreply.github.com>
2024-07-06 13:43:59 +0700

    Update Barcode_Page.dart

--- lib/staff/Barcode_Page.dart
+++ lib/staff/Barcode_Page.dart
@@ -10,8 +10,45 @@ class BarcodePage extends StatelessWidget {
           title: const Text('Notifications'),
           backgroundColor: const Color(0xffb29377),
         ),
-        body: Center(
-      child: Text('Coming Soon'),
-    ));
+        body: Consumer<RequestProvider>(
+        builder: (context, requestProvider, child) {
+          final notifications = requestProvider.userNotifications;
+
+          return ListView.builder(
+            padding: const EdgeInsets.all(16.0),
+            itemCount: notifications.length,
+            itemBuilder: (context, index) {
+              final request = notifications[index];
+              return Card(
+                margin: const EdgeInsets.only(bottom: 16.0),
+                child: ListTile(
+                  title: Text(
+                    request.status == 'accepted'
+                        ? 'Your request for ${request.itemName} has been accepted.'
+                        : 'Your request for ${request.itemName} has been rejected.',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  trailing: IconButton(
+                    icon: Icon(
+                      request.isRead
+                          ? Icons.mark_email_read
+                          : Icons.mark_email_unread,
+                      color: request.isRead ? Colors.green : Colors.red,
+                    ),
+                    onPressed: () {
+                      if (request.isRead) {
+                        requestProvider.markAsUnread(request);
+                      } else {
+                        requestProvider.markAsRead(request);
+                      }
+                    },
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
   }
 }
